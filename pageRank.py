dcb8b61c9133889a017645e24529bc8280a3698c
Adolfo Hernandez <adolhdzf@gmail.com>
2024-10-07 22:58:36 -0600

    fix: corrections on authors names

--- pageRank.py
+++ pageRank.py
@@ -1,6 +1,6 @@
 """
 @description: Page Rank for specific given nodes
-@authors: Aracelli Melissa Boza Zabarburú, Adolfo Hernández Fernández, Luis Enrique Salazar Perez 
+@authors: Aracelli Melissa Boza Zabarburú A01662934, Adolfo Hernández Fernández A01664412, Luis Enrique Salazar Pérez A00833460
 @date: 2024/07/10 
 """
 from collections import defaultdict
